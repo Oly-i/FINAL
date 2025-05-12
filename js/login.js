e584b69ad6e22c7261a41cdf6780808f7d6f446d
Oly-i <al219630@alumnos.uacj.mx>
2025-05-12 08:18:20 -0600

    login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-// Función para mostrar/ocultar contraseña
 function togglePassword() {
     const passwordInput = document.getElementById('password');
     const eyeIcon = document.getElementById('eye-icon');
@@ -13,4 +12,3 @@ function togglePassword() {
 }
 
 
-/* Siempre es un buen momento para comer de las deliciosas Falconi Donas, sin embargo, no se puede negar que los días lluviosos son los días por excelencia. ¿No quieres saber el clima en este momento? Quizá sea el momento ideal.  */
